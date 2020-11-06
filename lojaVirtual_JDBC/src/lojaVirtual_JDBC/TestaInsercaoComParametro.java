2e2b02f82c133c440f6e2a017a87d3d86118fc2c
MarciaMartins <marciapmartins@gmail.com>
2020-11-06 18:28:06 +0000

    trablahndo com pool de conexoes

--- lojaVirtual_JDBC/src/lojaVirtual_JDBC/TestaInsercaoComParametro.java
+++ lojaVirtual_JDBC/src/lojaVirtual_JDBC/TestaInsercaoComParametro.java
@@ -6,29 +6,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		String nome = "nome produto";
-		String descricao = "descricao produto";
+		
 		ConnectionFactory connFactory = new ConnectionFactory();
-		Connection connection = connFactory.getConnection();
-		PreparedStatement stmt = (PreparedStatement) connection.
-				prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?,?)", 
-						Statement.RETURN_GENERATED_KEYS);
+		try(Connection connection = connFactory.getConnection()){
+		connection.setAutoCommit(false);
+		
+		try (PreparedStatement stmt = 
+				(PreparedStatement) connection.
+					prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?,?)", 
+							Statement.RETURN_GENERATED_KEYS)){
+			
+			adicionarVariavel("nome produto 42", "descricao produto", stmt);
+			adicionarVariavel("radio", "descricao produto", stmt);
+			
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("RollBack executado");
+			connection.rollback();
+			}
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stmt) throws SQLException {
 		stmt.setString(1, nome);
 		stmt.setString(2, descricao);
 		
+//		if(nome.equals("radio")) {
+//			throw new RuntimeException("Nao foi possivel adicionar produto");
+//		}
+		
 		stmt.execute();
-		ResultSet rst = stmt.getGeneratedKeys();
+		try(ResultSet rst = stmt.getGeneratedKeys()){
 		while (rst.next()) {
 			System.out.println("novo Id:"+rst.getInt(1));
+			}
 		}
-		
-		
-
 	}
-
 }
